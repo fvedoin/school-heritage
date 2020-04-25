8b0630fdf7a0f84442194deb71d7189dfe6b8a49
Fernand <fernando.vgarcia@hotmail.com>
2020-04-25 18:08:59 -0300

    DataTable added on rooms page. Showing and hiding elements on rooms page.

---
+++ static/js/scripts/rooms-page.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+
+    //Input for searching
+    const searchField = $('#dt-search');
+    //Initially display only the list section
+    $('#form-create').hide();
+
+    $('#close-form').click(function () {
+        $('#form-create').hide();
+        $('#list').show();
+    });
+
+    $('#show-form-create').click(function () {
+        $('#form-create').show();
+        $('#list').hide();
+    });
+
+    //DataTable configuration
+    const dt = $('#dt').DataTable({
+        sDom: 'lrtip',
+        'pageLength': 10,
+        'lengthChange': false,
+        'info': true,
+        'columnDefs': [
+            { 'targets': 0, 'orderable': false },
+            { 'targets': 1, 'orderable': false, 'searchable': false }
+        ]
+    });
+
+    //Clean the filters
+    function restartSearch() {
+        dt.search(searchField.val()).columns().search('').draw();
+    }
+
+    //Looks for the match register on DataTable
+    function search() {
+        restartSearch();
+        if (searchField.val()) {
+            dt.search(searchField.val()).draw();
+        }
+    }
+
+    //Executes the search on key up
+    searchField.on('keyup', function () {
+        search();
+    });
+
+});
