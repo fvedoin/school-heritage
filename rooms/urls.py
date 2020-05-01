972ce41917ab8d59e347a616ec5d491e716156d0
Fernand <fernando.vgarcia@hotmail.com>
2020-05-01 17:39:55 -0300

    Editing and deleting rooms.

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, RoomEditView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('<int:pk>/', RoomEditView.as_view(), name='edit'),
 ]
