cd680f55a70f17b79c633e6e773698abe0895268
Bruno Frizzo <brunofrizzo@outlook.com>
2020-05-31 22:28:52 -0300

    log-page.js and problems-page.js adjustments.

---
+++ static/js/scripts/logs-page.js
@@ -0,0 +1,37 @@
+setInitialView();
+
+//Input for searching
+const searchField = $('#dt-search');
+
+//DataTable configuration
+const dt = $('#dt').DataTable({
+    sDom: 'lrtip',
+    'pageLength': 10,
+    'lengthChange': false,
+    'autoWidth': false,
+    'info': true,
+    'order': [[2, "desc"]],
+    'columnDefs': [
+        {'targets': [0,2], 'orderable': true},
+        {'targets': 1, 'orderable': false},
+        {'targets': 3, 'orderable': false, 'searchable': false, 'width': '53px', 'className': 'operation-column'}
+    ]
+});
+
+//Clean the filters
+function restartSearch() {
+    dt.search(searchField.val()).columns().search('').draw();
+}
+
+//Looks for the match register on DataTable
+function search() {
+    restartSearch();
+    if (searchField.val()) {
+        dt.search(searchField.val()).draw();
+    }
+}
+
+//Executes the search on key up
+searchField.on('keyup', function () {
+    search();
+});
