ca4d0d5afba6640bd706b3308985c7769aaeaf1f
Felipe <ficechin@hotmail.com>
2020-05-31 22:03:37 -0300

    Reports page adjusted.

---
+++ static/js/scripts/reports-page.js
@@ -0,0 +1,18 @@
+$('#input-periodo').hide();
+$(".data").mask("00/00/0000");
+$('#filtro').change(function () {
+    $('#input-periodo').hide();
+    if (this.value == 'periodo') {
+        $('#input-periodo').show();
+    }
+});
+$('.data').datepicker({
+    dateFormat: 'dd/mm/yy',
+    dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+    dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S', 'D'],
+    dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb', 'Dom'],
+    monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+    monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+    nextText: 'Próximo',
+    prevText: 'Anterior'
+});
